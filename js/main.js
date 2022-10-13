5555d6d87e0ca6d464adb135618f0cfce70452ef
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2022-10-13 20:42:40 +0400

    Генерирует фотографии

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
-const calculateComment = function (string, limit) {
-  if (string.length <= limit) {
-    return true;
+
+// function checkStringLength (string, length) {
+//   return string.length <= length;
+// }
+
+let index = 1;
+
+const descriptionPhotos = [
+  'cat',
+  'dog',
+  'mouse',
+  'horse',
+  'sheep',
+  'chicken',
+  'cow',
+  'pig',
+  'giraffe',
+  'kangaroo',
+  'elephant',
+  'lion',
+  'tiger',
+  'ostrich',
+  'hippopotamus',
+  'bear',
+  'fox',
+  'wolf',
+  'hare',
+  'snake',
+  'leopard',
+  'owl',
+  'eagle',
+  'donkey',
+  'camel'
+];
+
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
   }
-  return false;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-// return string.length <= limit;
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const even = function(number) {
-  if (!(number % 2)) {
-    return true;
-  }
-  return false;
-};
+const createPhoto = () => ({
+  url: `photos/${index++}.jpg`,
+  description: getRandomArrayElement(descriptionPhotos),
+  likes: getRandomPositiveInteger(15, 200),
+  comment: getRandomPositiveInteger(0, 200)
+});
 
+const similarPhotos = Array.from({length: 25}, createPhoto);
 
-const odd = function(number) {
-  if (number % 2) {
-    return true;
-  }
-  return false;
-};
-
-
-const random = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+// eslint-disable-next-line no-console
+console.log(similarPhotos);
