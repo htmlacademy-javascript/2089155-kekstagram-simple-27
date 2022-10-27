904265ae23fd2858a913c050d03979f46001611f
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2022-10-27 18:24:33 +0400

    Добавляет испавление ошибок в шаблонах

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-import {createPhotos} from './data.js';
-import './user-image.js';
+import { createPhotos } from './data.js';
+import { renderPictures } from './user-image.js';
 
 // eslint-disable-next-line no-console
 console.log(createPhotos());
+
+// eslint-disable-next-line no-console
+console.log(renderPictures());
--- js/user-images.js
+++ js/user-images.js
@@ -1,19 +1,24 @@
-import {createPhotos} from './data.js';
+import { createPhotos } from './data.js';
 
 const userImages = document.querySelector('.pictures');
 const similarImageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const similarImages = createPhotos();
 
-const DocumentFragment = document.createDocumentFragment();
+const renderPictures = () => {
 
-similarImages.forEach(({url,description,likes,comment}) => {
-  const imageElement = similarImageTemplate.cloneNode(true);
-  imageElement.querySelector('.picture__img').textContent = url;
-  imageElement.querySelector('.picture__info').textContent = description;
-  imageElement.querySelector('.picture__likes').textContent = likes;
-  imageElement.querySelector('.picture__comments').textContent = comment;
-  DocumentFragment.appendChild(imageElement);
-});
+  const DocumentFragment = document.createDocumentFragment();
 
-userImages.appendChild(DocumentFragment);
+  similarImages.forEach(({ url, description, likes, comment }) => {
+    const imageElement = similarImageTemplate.cloneNode(true);
+    imageElement.querySelector('.picture__img').src = url;
+    imageElement.querySelector('.picture__info').alt = description;
+    imageElement.querySelector('.picture__likes').textContent = likes;
+    imageElement.querySelector('.picture__comments').textContent = comment;
+    DocumentFragment.appendChild(imageElement);
+  });
+
+  userImages.appendChild(DocumentFragment);
+};
+
+export {renderPictures};
